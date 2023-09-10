1811e49b9023724cf64ccfe42557bbca4fe32c5d
AndrewHypster <andrewgrechukh123@gmail.com>
2023-09-10 13:06:22 +0300

    add descriptions

--- src/components/WorkPage.jsx
+++ src/components/WorkPage.jsx
@@ -14,30 +14,30 @@ const WorkPage = () => {
           <ColorT text={Works[id].title} indexes={Works[id].colorText[1]} color={Works[id].colorText[0]} />
         </h1>
         <div className="info">
-          <div>
+
+          <section>
             <div className="slider-img">
               <h3>SLIDER</h3>
             </div>
-            <div className="data">
+            <div className="data"></div>
+          </section>
 
-            </div>
-          </div>
-          <div>
+          <section>
             <div className="slider-info">
               <h2>
                 <ColorT text='Description' indexes={[0, 3, 6, 10]} color='blue' />
                 <div className='hr'></div>
               </h2>
               <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book
+                {Works[id].description.map(text => <p>{text}</p>)}
               </p>
-
             </div>
+
             <div className="buttons">
               <a href={Works[id].links.watch} target='_blank'><h3 className='Watch'>Watch</h3></a>
               <a href={Works[id].links.code} target='_blank'><h3 className='Code'>Code</h3></a>
             </div>
-          </div>
+          </section>
         </div>
       </div>
     </div>
--- src/components/some/SliderCards.jsx
+++ src/components/some/SliderCards.jsx
@@ -2,11 +2,12 @@ import { Link } from 'react-router-dom'
 import Cards from '../../jsons/MyWorks.json'
 
 const SliderCards = () => {
+  const scrollUp = () => window.scroll(0, 0)
 
   return (
     <div className="slider">
       {Cards.map((card, i) =>
-        <Link to={`MyWorks/${i}`}>
+        <Link to={`MyWorks/${i}`} onClick={scrollUp}>
           <div className="card" style={{backgroundImage: `url('${card.img}')`}} key={i}>
             <div className="text">
               <h1>{card.title}</h1>
